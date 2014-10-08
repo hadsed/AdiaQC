de532894bf0b9c94746cdd823d09875239310e74
Jonathan Schrock <jschrock@pod9.ccs.ornl.gov>
2014-10-08 15:58:43 -0400

    Cleaned up the code some and removed the executable from the tree.

--- C_simulator/simulator.c
+++ C_simulator/simulator.c
@@ -20,16 +20,15 @@
 // author: EvilTeach, Daniel Lyons
 /*
 const char *byte_to_binary(int x) {
-  static char b[9];
-  b[0] = '\0';
+    static char b[9];
+    b[0] = '\0';
 
-  int z;
-  for (z = 128; z > 0; z >>= 1)
-    {
-      strcat(b, ((x & z) == z) ? "1" : "0");
+    int z;
+    for( z = 128; z > 0; z >>= 1 ){
+        strcat( b, ( (x & z) == z ) ? "1" : "0" );
     }
 
-  return b;
+    return b;
 }
 */
 
@@ -40,7 +39,6 @@ inline void scaleVec( fftw_complex *vec, const double s, const uint64_t N ){
         vec[i] *= s;
     }
 }
-
 
 inline void expMatTimesVec( fftw_complex *vec, const double *mat, const fftw_complex cc, const uint64_t N ){
     uint64_t i;
@@ -54,35 +52,6 @@ inline void expMatTimesVec( fftw_complex *vec, const double *mat, const fftw_com
 //from: http://www.dreamincode.net/forums/topic/61496-find-n-max-elements-in-unsorted-array/
 //author: baavgai
 //date: 2014-09-08, 1630
-/*
-void addLarger( double value, double *list, const uint64_t size ){
-    uint64_t i = 0;
-    while( i < size-1 && value > list[i+1] ){
-        list[i++] = list[i+1];
-    }
-    list[i] = value;
-}
-
-//find max by magnitude
-void findLargest( double *listN, fftw_complex *list, const uint64_t size, const uint64_t N ){
-    uint64_t i;
-    double temp;
-    
-    //listNth = (int*) calloc (nLargest,sizeof(int));
-    for( i = 0; i < N; i++ ){
-        addLarger( cabs(list[i]), listN, N );
-    }
-    for( i = N; i < size; i++ ){
-        temp = cabs( list[i] );
-        if( temp > *listN ){
-            addLarger( temp, listN, N );
-        }
-    }
-    //dumpArray(listNth, nLargest);
-    //free(listNth);
-}
-*/
-
 void addLarger( double value, uint64_t indx, uint64_t *list, double *mag_list, const uint64_t size ){
     uint64_t i = 0;
     while( i < size-1 && value > mag_list[i+1] ){
@@ -149,50 +118,53 @@ int main( int argc, char **argv ){
                          Parse configuration file
     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
     //TODO: going to need logic to handle incomplete config files
-    //TODO: put all os this in it's own function?
-    JSON_Value *root_value = NULL;
-    JSON_Object *root_object;
-    JSON_Array *array;
-
     if( argc < 2 ){
         fprintf( stderr, "Need a json configuration file. Terminating...\n" );
         return 1;
     }
-    root_value = json_parse_file_with_comments( argv[1] );
-    root_object = json_value_get_object( root_value );
 
-    nQ = (uint64_t) json_object_dotget_number( root_object, "scalars.nq" );
-    prnt = json_object_dotget_boolean( root_object, "scalars.print" );
-    L = (uint64_t) json_object_dotget_number( root_object, "scalars.lrgs" );
-    T = json_object_dotget_number( root_object, "scalars.t" );
-    dt = json_object_dotget_number( root_object, "scalars.dt" );
+    /* Parse config file and populate appropriate data structures */
+    {
+        JSON_Value *root_value = NULL;
+        JSON_Object *root_object;
+        JSON_Array *array;
 
-    al   = (double *)malloc( nQ*sizeof(double) );
-    de   = (double *)malloc( nQ*sizeof(double) );
-    be   = (double *)malloc( (nQ*(nQ-1)/2)*sizeof(double) );
+        root_value = json_parse_file_with_comments( argv[1] );
+        root_object = json_value_get_object( root_value );
 
-    array = json_object_dotget_array( root_object, "coefficients.alpha" );
-    if( array != NULL ){
-        for( i = 0; i < json_array_get_count(array); i++ ){
-            al[i] = -json_array_get_number( array, i );
+        nQ = (uint64_t) json_object_dotget_number( root_object, "scalars.nq" );
+        prnt = json_object_dotget_boolean( root_object, "scalars.print" );
+        L = (uint64_t) json_object_dotget_number( root_object, "scalars.lrgs" );
+        T = json_object_dotget_number( root_object, "scalars.t" );
+        dt = json_object_dotget_number( root_object, "scalars.dt" );
+
+        al   = (double *)malloc( nQ*sizeof(double) );
+        de   = (double *)malloc( nQ*sizeof(double) );
+        be   = (double *)malloc( (nQ*(nQ-1)/2)*sizeof(double) );
+
+        array = json_object_dotget_array( root_object, "coefficients.alpha" );
+        if( array != NULL ){
+            for( i = 0; i < json_array_get_count(array); i++ ){
+                al[i] = -json_array_get_number( array, i );
+            }
         }
-    }
 
-    array = json_object_dotget_array( root_object, "coefficients.beta" );
-    if( array != NULL ){
-        for( i = 0; i < json_array_get_count(array); i++ ){
-            be[i] = -json_array_get_number( array, i );
+        array = json_object_dotget_array( root_object, "coefficients.beta" );
+        if( array != NULL ){
+            for( i = 0; i < json_array_get_count(array); i++ ){
+                be[i] = -json_array_get_number( array, i );
+            }
         }
-    }
 
-    array = json_object_dotget_array( root_object, "coefficients.delta" );
-    if( array != NULL ){
-        for( i = 0; i < json_array_get_count(array); i++ ){
-            de[i] = -json_array_get_number( array, i );
+        array = json_object_dotget_array( root_object, "coefficients.delta" );
+        if( array != NULL ){
+            for( i = 0; i < json_array_get_count(array); i++ ){
+                de[i] = -json_array_get_number( array, i );
+            }
         }
-    }
 
-    json_value_free( root_value );
+        json_value_free( root_value );
+    }
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         Compute the Hamiltonian and state vector for the simulation
@@ -204,7 +176,6 @@ int main( int argc, char **argv ){
     */
     dim = 1 << nQ;
     factor = 1.0/sqrt( dim );
-    /* factor = 1.0; */
     
     fft_dims = (int *)malloc( nQ*sizeof(int) );
     psi  = (fftw_complex *)malloc( (dim)*sizeof(fftw_complex) );
@@ -217,53 +188,13 @@ int main( int argc, char **argv ){
 
     plan = fftw_plan_dft( nQ, fft_dims, psi, psi, FFTW_FORWARD, FFTW_MEASURE );
 
-    /* for( j = 0; j < dim; j++ ){ */
-    /*   printf( "be[%d]: %f\n", j, be[j]); */
-    /* } */
     /*
         Assemble Hamiltonian and state vector
     */
-    //TODO: put all of this into its own function?
-    /*
-    flag2 = 1;
-    uint64_t bcount = 0;
-    for( i = 0; i < nQ; i++ ){
-        testi = 1 << (nQ - i - 1);
-
-        flag = 1;
-        for( j = i; j < nQ; j++ ){
-            testj = 1 << (nQ - j - 1);
-            dzi = dzj = -1;
-
-            //TODO: when parallel, only go through local indices and such
-            for( k = 0; k < dim; k++ ){
-                dzi = (k % testi == 0) ? (-1)*dzi : dzi;
-                if( flag ){
-                    hz[k] += al[i] * dzi;
-                    hhxh[k] += de[i] * dzi;
-                } else {
-                    dzj = (k % testj == 0) ? (-1)*dzj : dzj;
-                    //hz[k] += be[i][j] * dzi * dzj;
-                    hz[k] += be[bcount] * dzi * dzj;
-                    //bcount++;
-                }
-                if( flag2 ){
-                    psi[k] = factor;
-                }
-            }
-            if( !flag ){
-                bcount++;
-            }
-            flag = 0;
-            flag2 = 0;
-        }
-    }
-    */
-
     uint64_t bcount;
     for( k = 0; k < dim; k++ ){
-
         //TODO: when parallelized, k in dzi test will be ~(k + base)
+
         bcount = 0;
         for( i = 0; i < nQ; i++ ){
             testi = 1 << (nQ - i - 1);
@@ -290,11 +221,6 @@ int main( int argc, char **argv ){
     fftw_complex cz, cx;
     double t;
     N = (uint64_t)(T / dt);
-    //plan = fftw_plan_dft( nQ, fft_dims, psi, psi, FFTW_FORWARD, FFTW_ESTIMATE );
-    //plan = fftw_plan_dft( nQ, fft_dims, psi, psi, FFTW_FORWARD, FFTW_MEASURE );
-    /* for( j = 0; j < dim; j++ ){ */
-    /*   printf( "hz[%d]: %f\n", j, hz[j]); */
-    /* } */
     for( i = 0; i < N; i++ ){
         t = i*dt;
         //t0 = (i-1)*dt;
@@ -305,26 +231,13 @@ int main( int argc, char **argv ){
 
         //Evolve system
         expMatTimesVec( psi, hz, cz, dim ); //apply Z part
-        /* for( j = 0; j < dim; j++ ){ */
-        /*     printf( "before: psi[%d] = (%f, %f)\t%f\n", j, creal(psi[j]), cimag(psi[j]), cabs(psi[j]*psi[j]) ); */
-        /* } */
         fftw_execute( plan );
-        /* for( j = 0; j < dim; j++ ){ */
-        /*     printf( "after: psi[%d] = (%f, %f)\t%f\n", j, creal(psi[j]), cimag(psi[j]), cabs(psi[j]*psi[j]) ); */
-        /* } */
-        //_fwht( psi, 0, dim );
         expMatTimesVec( psi, hhxh, cx, dim ); //apply X part
         fftw_execute( plan );
-        //_fwht( psi, 0, dim );
         expMatTimesVec( psi, hz, cz, dim ); //apply Z part
         scaleVec( psi, 1.0/dim, dim );
-        /* for( j = 0; j < dim; j++ ){ */
-        /*     printf( "psi[%d] = (%f, %f)\t%f\n", j, creal(psi[j]), cimag(psi[j]), cabs(psi[j]*psi[j]) ); */
-        /* } */
     }
     fftw_destroy_plan( plan );
-    //TODO: need to figure out where to do this, beginning or here
-    //    scaleVec( psi, 1.0/sqrt(dim), dim );
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
                         Check solution and clean up
@@ -336,26 +249,23 @@ int main( int argc, char **argv ){
         for( i = 0; i < dim; i++ ){
             printf( "psi[%d] = (%f, %f)\t%f\n", 
 		    i,
-		    creal(psi[i]), 
-		    cimag(psi[i]), 
-		    cabs(psi[i]*psi[i])
-		    /*byte_to_binary(i)*/ );
+		    creal( psi[i] ), 
+		    cimag( psi[i] ), 
+		    cabs( psi[i]*psi[i] ) );
         }
     } else {
         uint64_t *largest = (uint64_t *)calloc( L, sizeof(uint64_t) );
         findLargest( largest, psi, dim, L );
         for( i = 0; i < L; ++i ){
-            printf( "%1.10f\t%d\tpsi[%d] = (%f, %f)\t\n",
-		    /*byte_to_binary(largest[i]),*/
-		    cabs( psi[largest[L-1-i]]*psi[largest[L-1-i]] ),
+            printf( "psi[%d] = (%f, %f)\t%f\n",
 		    i,
-		    largest[L-1-i],
 		    creal( psi[largest[L-1-i]] ), 
-		    cimag( psi[largest[L-1-i]] )
-		     );
+		    cimag( psi[largest[L-1-i]] ),
+		    cabs( psi[largest[L-1-i]]*psi[largest[L-1-i]] ) );
         }
         free( largest );
     }
+
     /*
         Free work space.
     */
