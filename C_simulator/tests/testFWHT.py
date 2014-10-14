c1d3a88ee82f967de1bb5d9feb729e2da01cb7be
Jonathan Schrock <jschrock@pod9.ccs.ornl.gov>
2014-10-14 16:36:14 -0400

    Removed fftw dependency from simulator.c and edited a couple of my test codes

--- C_simulator/tests/testFWHT.py
+++ C_simulator/tests/testFWHT.py
@@ -13,6 +13,29 @@ def _fwht( vec, start, end ):
         _fwht( vec, start+iters, end ) #lower
 
 
+def FWHTI( vec, n ):
+    iter = 1
+
+    while( iter <= n ):
+        temp = 2**(n-iter)
+        i = 0
+        while( i < 2**n ):
+        #for i in xrange( 0, 2**n ):
+            #if( i//temp % 2 == 1 ):
+            #    vec[i] = vec[i - temp] - 2*vec[i]
+            #else:
+            #    vec[i] += vec[i + temp]
+
+            veci = vec[i]
+            vec[i] += vec[i + temp]
+            vec[i + temp] = veci - vec[i + temp]
+            if( (i+1)//temp % 2 == 1 ):
+                i += temp + 1
+            else:
+                i += 1
+        iter += 1
+
+            
 def FWHT( vec, n ):
     _fwht( vec, 0, 2**n )
 
@@ -20,6 +43,7 @@ def FWHT( vec, n ):
 if __name__ == "__main__":
     vec = [1,0,1,0,0,1,1,0]
 
-    FWHT( vec, 3 )
+    FWHTI( vec, 3 )
+    FWHTI( vec, 3 )
 
-    print np.array(vec)
+    print (1/8.0)*np.array(vec)
