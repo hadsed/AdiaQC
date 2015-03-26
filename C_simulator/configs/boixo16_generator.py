C_simulator: Fixed 16-qubit Boixo case, it runs correctly now. Also extended the printout precision in simulator.c to be about 16 digits, better for debugging.

C_simulator/configs/boixo16_generator.py
@@ -5,16 +5,16 @@ import numpy as np
 loaded = np.loadtxt('boixo16.txt')
 # Construct Ising matrix
 J = np.zeros((16,16))
-# this simulator has a negative somewhere
 for i,j,val in loaded:
-    J[i-1,j-1] = -1.0*val
+    J[i-1,j-1] = val
 # get diagonal, apply bias
-alpha = 0.44*np.diag(J)
+alpha = np.diag(J).copy()
+alpha[:8] *= 0.44
 beta = []
 # go through upper triangle, no diags
 for row in xrange(16):
     for col in xrange(row+1,16):
-        beta.append(J[row,col])
+        beta.append(-1.0*J[row,col])
 # this never changes really
 delta = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 
                   1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
@@ -23,8 +23,8 @@ jout = {
     "scalars": {
         "nq" : 16,
         "lrgs" : 10,
-        "t" : 400.0,
-        "dt" : 0.1
+        "t" : 500.0,
+        "dt" : 0.01
     },
     "coefficients" : {
         "alpha" : list(alpha),